14d65cca735b99548dc1d87499a226d66f5d7604
Thomas Lohmueller <thomas@lohmueller.ch>
2012-10-28 15:28:01 +0100

    No changes - just re-indented everything.
    Now using 4 spaces to indent code.

--- RF12_T3.cpp
+++ RF12_T3.cpp
@@ -23,7 +23,7 @@ struct drssi_dec_t {
 };
 
 const drssi_dec_t drssi_dec_tree[] = {
-            /*  up    down  thres*/
+    /*  up    down  thres*/
     /* 0 */ { B1001, B1000, B000 },
     /* 1 */ { B0010, B0000, B001 },
     /* 2 */ { B1011, B1010, B010 },
@@ -46,49 +46,49 @@ const drssi_dec_t drssi_dec_tree[] = {
  * Return: void
  */
 int RF12_T3::reinit(uint8_t id, uint8_t band, uint8_t group, uint8_t rate) {
-  nodeId = id;
-  groupId= group;
-  bandId = band;
-  datarate = rate;
-  available = 0;
-  reportBroken = 0;
-
-  // configure SPI pins
-  digitalWriteFast(SCK, LOW);
-  digitalWriteFast(MOSI, LOW);
-  digitalWriteFast(SS, HIGH);
-  pinMode(SCK, OUTPUT);
-  pinMode(MOSI, OUTPUT);
-  pinMode(SS, OUTPUT); // is pin 10
-
-  // enables and configures SPI module
-  SIM_SCGC6 |= SIM_SCGC6_SPI0;  // enable SPI clock
-  SPI0_MCR = 0x80004000;
-  SPCR |= _BV(MSTR);
-  SPCR |= _BV(SPE);
-  SPCR &= ~(_BV(DORD));   // MSBFIRST SPI
-
-  // 16MHz 16bit transfers on CTAR0
-  SPI0_CTAR0 = 0xF8010000;
-
-  // 2MHz 8bit transfers on CTAR1 (for reading FIFO)
-  SPI0_CTAR1 = 0x38010002;
-
-  // init some values
-  _index = 0;
-  buffer[0] = 0;  // reset header
-  buffer[1] = 0;  // reset len
-
-  // ARSSI
-  pinMode(A0, INPUT);
+    nodeId = id;
+    groupId= group;
+    bandId = band;
+    datarate = rate;
+    available = 0;
+    reportBroken = 0;
     
-  // register irq
-  pinMode(irqLine, INPUT);
-  attachInterrupt(irqLine, RF12_T3::_handleIrq4, LOW);
-
-  // requesting RFM12b reset
-  rf12_xfer(0xCA82); // enable software reset
-  rf12_xfer(0xFE00); // do software reset
+    // configure SPI pins
+    digitalWriteFast(SCK, LOW);
+    digitalWriteFast(MOSI, LOW);
+    digitalWriteFast(SS, HIGH);
+    pinMode(SCK, OUTPUT);
+    pinMode(MOSI, OUTPUT);
+    pinMode(SS, OUTPUT); // is pin 10
+    
+    // enables and configures SPI module
+    SIM_SCGC6 |= SIM_SCGC6_SPI0;  // enable SPI clock
+    SPI0_MCR = 0x80004000;
+    SPCR |= _BV(MSTR);
+    SPCR |= _BV(SPE);
+    SPCR &= ~(_BV(DORD));   // MSBFIRST SPI
+    
+    // 16MHz 16bit transfers on CTAR0
+    SPI0_CTAR0 = 0xF8010000;
+    
+    // 2MHz 8bit transfers on CTAR1 (for reading FIFO)
+    SPI0_CTAR1 = 0x38010002;
+    
+    // init some values
+    _index = 0;
+    buffer[0] = 0;  // reset header
+    buffer[1] = 0;  // reset len
+    
+    // ARSSI
+    pinMode(A0, INPUT);
+    
+    // register irq
+    pinMode(irqLine, INPUT);
+    attachInterrupt(irqLine, RF12_T3::_handleIrq4, LOW);
+    
+    // requesting RFM12b reset
+    rf12_xfer(0xCA82); // enable software reset
+    rf12_xfer(0xFE00); // do software reset
 }
 
 
@@ -99,11 +99,11 @@ int RF12_T3::reinit(uint8_t id, uint8_t band, uint8_t group, uint8_t rate) {
  * Return: From RFM12 module received data
  */
 inline uint16_t RF12_T3::rf12_xfer(uint16_t data) {
-  digitalWriteFast(10, LOW);
-  SPI0_PUSHR = (1<<26) | data;    // send data (clear transfer counter)
-  while (! SPI0_TCR) ; // loop until transfer is complete
-  digitalWriteFast(10, HIGH);
-  return SPI0_POPR;
+    digitalWriteFast(10, LOW);
+    SPI0_PUSHR = (1<<26) | data;    // send data (clear transfer counter)
+    while (! SPI0_TCR) ; // loop until transfer is complete
+    digitalWriteFast(10, HIGH);
+    return SPI0_POPR;
 }
 
 
@@ -114,132 +114,132 @@ inline uint16_t RF12_T3::rf12_xfer(uint16_t data) {
  * Return: void
  */
 void RF12_T3::handleIrq() {
-  // check if we really have to do something
-  if (digitalRead(4) == HIGH)
-    return;
-
-  // reading state
-  digitalWriteFast(10, LOW);  // select RFM12b module
-  SPI0_PUSHR = (1<<26) | 0x0000;    // send data (clear transfer counter)
-  while (! SPI0_TCR) ; // loop until transfer is complete
-  uint16_t res = SPI0_POPR;
-
-  // fifo full or buffer empty
-  if (res & 0x8000) {
+    // check if we really have to do something
+    if (digitalRead(4) == HIGH)
+        return;
+    
+    // reading state
+    digitalWriteFast(10, LOW);  // select RFM12b module
+    SPI0_PUSHR = (1<<26) | 0x0000;    // send data (clear transfer counter)
+    while (! SPI0_TCR) ; // loop until transfer is complete
+    uint16_t res = SPI0_POPR;
+    
+    // fifo full or buffer empty
+    if (res & 0x8000) {
+        
+        // =====================================================
+        // FIFO has a byte to read
+        if (state == RF_RECV) {
+            SPI0_PUSHR = (1<<28) | (1<<26); // CTAR1 transfer (slow 8bit), clear transfer counter
+            while (! SPI0_TCR) ; // loop until transfer is complete
+            uint8_t data = (uint8_t) SPI0_POPR;
+            digitalWriteFast(10, HIGH);
+            
+            // do drssi binary-tree search
+            if ( drssi < 6 ) {       // not yet final value
+                if ( bitRead(res,8) )  // rssi over threashold?
+                    drssi = drssi_dec_tree[drssi].up;
+                else
+                    drssi = drssi_dec_tree[drssi].down;
+                if ( drssi < 6 ) {     // not yet final destination
+                    rf12_xfer(0x94A0 | drssi_dec_tree[drssi].threshold);
+                }
+            }
+            
+            // save data to internal buffer
+            buffer[_index++] = data;
+            
+            if (_index==1) {          // first packet!
+                arssi = analogRead(A0);
+                rf12_crc = crc16_update(0xffff, groupId); // network group id
+                rf12_crc = crc16_update(rf12_crc, data);
+                
+            } else if (_index==2) {   // second packet (with length)
+                rf12_crc = crc16_update(rf12_crc, data);
+                
+            } else {                  // data (or crc)
+                rf12_crc = crc16_update(rf12_crc, data);
+                
+                // abort reception if we got a full packet
+                if (_index > buffer[1] + 3) { // +1 for header, +2 for checksum
+                    disableReceiver();
+                    _recvDone = 1;
+                }
+            }
+            
+            // =====================================================
+            // Buffer needs neyt byte to send
+        } else {
+            digitalWriteFast(10, HIGH);
+            
+            rf12_xfer(0xB800 | _toSend);
+            
+            // prepare next byte to send
+            if (state < 0) {
+                _toSend = buffer[2 + buffer[1] + state++];
+                rf12_crc = crc16_update(rf12_crc, _toSend);
+            } else {
+                switch (++state) {
+                    case RF_TXSYN1: _toSend = 0x2D; break;
+                    case RF_TXSYN2: _toSend = groupId;
+                        rf12_crc = crc16_update(0xffff, groupId);
+                        state = - (2 + buffer[1]);
+                        break;
+                    case RF_TXCRC1: _toSend = 0xff & rf12_crc; break;
+                    case RF_TXCRC2: _toSend = 0xff & (rf12_crc>>8);   break;
+                    case RF_TXTAIL1: _toSend = 0xAA; break; // dummy
+                    case RF_TXTAIL2: break; // dummy
+                    case RF_TXDONE: _toSend = 0x99; // dummy, fall through
+                    default:        disableTransmitter(); state = RF_IDLE;  // make sure we're back on track
+                }
+            }
+        }
+    } else
+        digitalWriteFast(10, HIGH);  // don't forget to disable CS to RFM module
     
     // =====================================================
-    // FIFO has a byte to read
-    if (state == RF_RECV) {
-      SPI0_PUSHR = (1<<28) | (1<<26); // CTAR1 transfer (slow 8bit), clear transfer counter
-      while (! SPI0_TCR) ; // loop until transfer is complete
-      uint8_t data = (uint8_t) SPI0_POPR;
-      digitalWriteFast(10, HIGH);
-
-      // do drssi binary-tree search
-      if ( drssi < 6 ) {       // not yet final value
-        if ( bitRead(res,8) )  // rssi over threashold?
-          drssi = drssi_dec_tree[drssi].up;
-        else
-          drssi = drssi_dec_tree[drssi].down;
-        if ( drssi < 6 ) {     // not yet final destination
-          rf12_xfer(0x94A0 | drssi_dec_tree[drssi].threshold);
-        }
-      }
-        
-      // save data to internal buffer
-      buffer[_index++] = data;
-
-      if (_index==1) {          // first packet!
-        arssi = analogRead(A0);
-        rf12_crc = crc16_update(0xffff, groupId); // network group id
-        rf12_crc = crc16_update(rf12_crc, data);
-
-      } else if (_index==2) {   // second packet (with length)
-        rf12_crc = crc16_update(rf12_crc, data);
-
-      } else {                  // data (or crc)
-        rf12_crc = crc16_update(rf12_crc, data);
-
-        // abort reception if we got a full packet
-        if (_index > buffer[1] + 3) { // +1 for header, +2 for checksum
-          disableReceiver();
-          _recvDone = 1;
-        }
-      }
-
+    // Power-On reset complete, do init now
+    if (res & 0x4000) {
+        rf12_xfer(0x80C7 | (bandId << 4)); // configuration settings
+        rf12_xfer(0x82D9);
+        rfMode = 0x82D9; rfMode = 0x82D9; // rx enabled, wakeup disabled, lowbat disabled
+        rf12_xfer(0xA640);
+        rf12_xfer(0xC600 | datarate);
+        rf12_xfer(0x94A3);
+        rf12_xfer(0xC2AC);
+        rf12_xfer(0xCA83);
+        rf12_xfer(0xCE00 | groupId); // sync Byte (group id)
+        rf12_xfer(0xC483);
+        rf12_xfer(0x9850);
+        rf12_xfer(0xCC57);
+        //rf12_xfer(0xE000); // no wakeup timer
+        rf12_xfer(0xC800);
+        //rf12_xfer(0xC049); // no lowbat detection
+        available = 1;
+        state = RF_IDLE;
+    }
+    
     // =====================================================
-    // Buffer needs neyt byte to send
-    } else {
-      digitalWriteFast(10, HIGH);
-
-      rf12_xfer(0xB800 | _toSend);
-      
-      // prepare next byte to send
-      if (state < 0) {
-        _toSend = buffer[2 + buffer[1] + state++];
-        rf12_crc = crc16_update(rf12_crc, _toSend);
-      } else {
-        switch (++state) {
-          case RF_TXSYN1: _toSend = 0x2D; break;
-          case RF_TXSYN2: _toSend = groupId;
-                          rf12_crc = crc16_update(0xffff, groupId);
-                          state = - (2 + buffer[1]);
-                          break;
-          case RF_TXCRC1: _toSend = 0xff & rf12_crc; break;
-          case RF_TXCRC2: _toSend = 0xff & (rf12_crc>>8);   break;
-          case RF_TXTAIL1: _toSend = 0xAA; break; // dummy
-          case RF_TXTAIL2: break; // dummy
-          case RF_TXDONE: _toSend = 0x99; // dummy, fall through
-          default:        disableTransmitter(); state = RF_IDLE;  // make sure we're back on track
+    // Wakeup-call from RFM12b module
+    if (res & 0x1000) { // wakeup-call, mark as received and switch off wakeup-timer
+        wakeup = 1;
+        rf12_sleep(0);
+    }
+    
+    // =====================================================
+    // FIFO overflow or buffer underrun
+    if (res & 0x2000) {
+        if (bitRead(rfMode, 7)) {  // we are receiving
+            resetReceiveBuffer();
+            disableReceiver();
+            enableReceiver();
+            
+        } else {                  // we are sending
+            // Resending could jam the air... so just abort sending
+            // TODO: recheck when sendWait() is implemented to not hang here
+            disableTransmitter();
         }
-      }
     }
-  } else
-    digitalWriteFast(10, HIGH);  // don't forget to disable CS to RFM module
-
-  // =====================================================
-  // Power-On reset complete, do init now
-  if (res & 0x4000) {
-    rf12_xfer(0x80C7 | (bandId << 4)); // configuration settings
-    rf12_xfer(0x82D9); 
-    rfMode = 0x82D9; rfMode = 0x82D9; // rx enabled, wakeup disabled, lowbat disabled
-    rf12_xfer(0xA640);
-    rf12_xfer(0xC600 | datarate);
-    rf12_xfer(0x94A3);
-    rf12_xfer(0xC2AC);
-    rf12_xfer(0xCA83);
-    rf12_xfer(0xCE00 | groupId); // sync Byte (group id)
-    rf12_xfer(0xC483);
-    rf12_xfer(0x9850);
-    rf12_xfer(0xCC57);
-    //rf12_xfer(0xE000); // no wakeup timer
-    rf12_xfer(0xC800);
-    //rf12_xfer(0xC049); // no lowbat detection
-    available = 1;
-    state = RF_IDLE;
-  }
-
-  // =====================================================
-  // Wakeup-call from RFM12b module
-  if (res & 0x1000) { // wakeup-call, mark as received and switch off wakeup-timer
-    wakeup = 1;
-    rf12_sleep(0);
-  }
-
-  // =====================================================
-  // FIFO overflow or buffer underrun
-  if (res & 0x2000) {
-    if (bitRead(rfMode, 7)) {  // we are receiving
-      resetReceiveBuffer();
-      disableReceiver();
-      enableReceiver();
-      
-    } else {                  // we are sending
-      // Resending could jam the air... so just abort sending
-      // TODO: recheck when sendWait() is implemented to not hang here
-      disableTransmitter();
-    }
-  }
 }
 
 
@@ -251,17 +251,17 @@ void RF12_T3::handleIrq() {
  * Return: void
  */
 void RF12_T3::enableReceiver() {
-  // init drssi detection tree
-  drssi = 3;
-  rf12_xfer(0x94A0 | drssi_dec_tree[drssi].threshold);
-
-  // switch RFM12b state
-  bitSet(rfMode,  7);   // enable receiver
-  bitSet(rfMode,  6);   // enable recv. baseband
-  bitClear(rfMode,5);   // disable transmitter
-  rf12_xfer(rfMode);
-  
-  state = RF_RECV;
+    // init drssi detection tree
+    drssi = 3;
+    rf12_xfer(0x94A0 | drssi_dec_tree[drssi].threshold);
+    
+    // switch RFM12b state
+    bitSet(rfMode,  7);   // enable receiver
+    bitSet(rfMode,  6);   // enable recv. baseband
+    bitClear(rfMode,5);   // disable transmitter
+    rf12_xfer(rfMode);
+    
+    state = RF_RECV;
 }
 
 
@@ -272,8 +272,8 @@ void RF12_T3::enableReceiver() {
  * Return: void
  */
 void RF12_T3::disableReceiver() {
-  bitClear(rfMode,7);
-  rf12_xfer(rfMode);
+    bitClear(rfMode,7);
+    rf12_xfer(rfMode);
 }
 
 
@@ -286,14 +286,14 @@ void RF12_T3::disableReceiver() {
  * Return: void
  */
 void RF12_T3::enableTransmitter() {
-  bitSet(rfMode, 5);   // enable transmitter
-  bitClear(rfMode, 7); // disable receiver
-  bitClear(rfMode, 6); // disable recv. baseband
-  rf12_xfer(rfMode);
-  
-  // prepare first byte (sync) to send and switch to correct state
-  _toSend = 0xCC;
-  state = RF_TXPRE; 
+    bitSet(rfMode, 5);   // enable transmitter
+    bitClear(rfMode, 7); // disable receiver
+    bitClear(rfMode, 6); // disable recv. baseband
+    rf12_xfer(rfMode);
+    
+    // prepare first byte (sync) to send and switch to correct state
+    _toSend = 0xCC;
+    state = RF_TXPRE;
 }
 
 
@@ -305,9 +305,9 @@ void RF12_T3::enableTransmitter() {
  * Return: void
  */
 void RF12_T3::disableTransmitter() {
-  bitClear(rfMode, 5);  // disable transmitter
-  rf12_xfer(rfMode);
-  state = RF_IDLE;
+    bitClear(rfMode, 5);  // disable transmitter
+    rf12_xfer(rfMode);
+    state = RF_IDLE;
 }
 
 
@@ -319,13 +319,13 @@ void RF12_T3::disableTransmitter() {
  * Return: true if sending is possible/allowed
  */
 boolean RF12_T3::canSend() {
-  if (available & (state == RF_IDLE || state == RF_RECV)) {
-    disableReceiver();
-    state = RF_IDLE;
-    return true;
-  } else {
-    return false;
-  }
+    if (available & (state == RF_IDLE || state == RF_RECV)) {
+        disableReceiver();
+        state = RF_IDLE;
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
@@ -336,7 +336,7 @@ boolean RF12_T3::canSend() {
  * Return: void
  */
 void RF12_T3::sendStart(uint8_t hdr) {
-  sendStart(hdr, 0, 0);
+    sendStart(hdr, 0, 0);
 }
 
 
@@ -349,12 +349,12 @@ void RF12_T3::sendStart(uint8_t hdr) {
  * Return: void
  */
 void RF12_T3::sendStart(uint8_t hdr, const void *ptr, uint8_t len) {
-  state = RF_IDLE;
-  buffer[0] = hdr;
-  buffer[1] = len;
-  memcpy((void*) &buffer[2], ptr, len);
-  rf12_crc = crc16_update(0xffff, groupId);
-  enableTransmitter();
+    state = RF_IDLE;
+    buffer[0] = hdr;
+    buffer[1] = len;
+    memcpy((void*) &buffer[2], ptr, len);
+    rf12_crc = crc16_update(0xffff, groupId);
+    enableTransmitter();
 }
 
 
@@ -366,12 +366,12 @@ void RF12_T3::sendStart(uint8_t hdr, const void *ptr, uint8_t len) {
  * Return: new crc value
  */
 inline uint16_t RF12_T3::crc16_update(uint16_t crc, uint8_t a) {
-  crc ^= a;
-  for (uint8_t i = 0; i < 8; ++i) {
-    if (crc & 1)  crc = (crc >> 1) ^ 0xA001;
-    else          crc = (crc >> 1);
-  }
-  return crc;
+    crc ^= a;
+    for (uint8_t i = 0; i < 8; ++i) {
+        if (crc & 1)  crc = (crc >> 1) ^ 0xA001;
+        else          crc = (crc >> 1);
+    }
+    return crc;
 }
 
 
@@ -385,28 +385,28 @@ inline uint16_t RF12_T3::crc16_update(uint16_t crc, uint8_t a) {
  * Return: void
  */
 inline void RF12_T3::rf12_sleep(unsigned long m) {
-  // calculate parameters for RFM12 module
-  // T_wakeup[ms] = m * 2^r
-  char r=0;
-  while (m > 255) {
-    r  += 1;
-    m >>= 1;
-  }
-
-  // Disable old one if present
-  if (bitRead(rfMode,1)) {  
-    bitClear(rfMode,1);
-    rf12_xfer(rfMode);
-  }
-  
-  // enable wakeup call if we have to
-  if (m>0) {
-    bitSet(rfMode,1);
-    rf12_xfer(rfMode);
-  
-    // write time to wakeup-register
-    rf12_xfer(0xE000 | (r<<8) | m);
-  }
+    // calculate parameters for RFM12 module
+    // T_wakeup[ms] = m * 2^r
+    char r=0;
+    while (m > 255) {
+        r  += 1;
+        m >>= 1;
+    }
+    
+    // Disable old one if present
+    if (bitRead(rfMode,1)) {  
+        bitClear(rfMode,1);
+        rf12_xfer(rfMode);
+    }
+    
+    // enable wakeup call if we have to
+    if (m>0) {
+        bitSet(rfMode,1);
+        rf12_xfer(rfMode);
+        
+        // write time to wakeup-register
+        rf12_xfer(0xE000 | (r<<8) | m);
+    }
 }
 
 
@@ -417,9 +417,9 @@ inline void RF12_T3::rf12_sleep(unsigned long m) {
  * Return: true if wakeup-call was received
  */
 inline boolean RF12_T3::gotWakeup() {
-  if (wakeup) {
-    wakeup = 0;
-    return 1;
-  }
-  return 0;
+    if (wakeup) {
+        wakeup = 0;
+        return 1;
+    }
+    return 0;
 }
--- RF12_T3.h
+++ RF12_T3.h
@@ -18,19 +18,19 @@
 
 
 class RF12_T3 {
- 
-  public:
-
+    
+public:
+    
     // =====================================================
     // Init
-
+    
     // returns instance for IRQ pin 4
     static RF12_T3* irqLine4() {
-      if (!_instance) {
-        _instance = (RF12_T3*)malloc(sizeof(RF12_T3));
-        _instance->setIrq(4);
-      }
-      return _instance;
+        if (!_instance) {
+            _instance = (RF12_T3*)malloc(sizeof(RF12_T3));
+            _instance->setIrq(4);
+        }
+        return _instance;
     }
     
     // Inits RFM12b
@@ -38,70 +38,70 @@ class RF12_T3 {
     int reinit(uint8_t id, uint8_t band, uint8_t group) {
         return reinit(id, band, group, 0x06); // JeeNode datarate
     }
-
-
+    
+    
     // =====================================================
     // Query network infos from last reinit
     
     uint8_t geNodetId(){ return nodeId; }
     uint8_t getGroup() { return groupId; }
     uint8_t getBand()  { return bandId; }
-
-
+    
+    
     // =====================================================
     // Receiving data
-
+    
     // Variables containing received data (TODO: Should be private)
     volatile uint8_t  _index;
     volatile char buffer[70];  // also used for sending
     volatile uint16_t rf12_crc;    // also used for sending
-
+    
     // check if there is a message waiting
     boolean recvDone() {
-      if (state == RF_IDLE) {
-        resetReceiveBuffer();
-        enableReceiver();
-        state=RF_RECV;
-        _recvDone = 0;
-      } else if (_recvDone) {
-        state = RF_IDLE;
-        if ( (!reportBroken && rf12_crc!=0x0000)  // report broken packets?
-           | (buffer[0] & (1<<6) && (buffer[0] & 0x1F)!=nodeId) ) // not for us
-          _recvDone = 0;  // ignore packet, because of invalid checksum
-      }
-      return _recvDone;
+        if (state == RF_IDLE) {
+            resetReceiveBuffer();
+            enableReceiver();
+            state=RF_RECV;
+            _recvDone = 0;
+        } else if (_recvDone) {
+            state = RF_IDLE;
+            if ( (!reportBroken && rf12_crc!=0x0000)  // report broken packets?
+                | (buffer[0] & (1<<6) && (buffer[0] & 0x1F)!=nodeId) ) // not for us
+                _recvDone = 0;  // ignore packet, because of invalid checksum
+        }
+        return _recvDone;
     }
     
     boolean getReportBroken() {
-      return reportBroken;
+        return reportBroken;
     }
-
+    
     // enable reporting of packages with invalid CRC
     void setReportBroken(boolean report) {
-      reportBroken = report;
+        reportBroken = report;
     }
     
     // return signal strength measured on analog input
     uint16_t getARRSI() {
-      return map(arssi, 0,1024, 0, 3301); // map analog value to mV
+        return map(arssi, 0,1024, 0, 3301); // map analog value to mV
     }
-
+    
     // return signal strength calculated out of DRSSI bit
     int8_t getDRSSI() {
-      if (! drssi & B1000)
-        return 0;
-    
+        if (! drssi & B1000)
+            return 0;
+        
         const int8_t table[] = {-106, -100, -94, -88, -82, -76, -70};
         return table[drssi & B111];
     }
-
-
+    
+    
     // =====================================================
     // Transmitting data
-
+    
     // Check if we can send now - required before doing sendStart(*)
     boolean canSend();
-
+    
     // Send empty packet with specific header
     void sendStart(uint8_t hdr);
     
@@ -110,62 +110,62 @@ class RF12_T3 {
     
     // wait until data was sent (TODO: implement!)
     void sendWait (uint8_t mode);
-
-
+    
+    
     // =====================================================
     // Wakeup-Timer
-
+    
     // set wakeup-timer
     inline void rf12_sleep (unsigned long millis);
-
+    
     // check if we got wakeup call
     inline boolean gotWakeup();
-
-
+    
+    
     // =====================================================
     // CRC (does not touch internal state, so public)
     inline static uint16_t crc16_update(uint16_t crc, uint8_t a);
-
-
+    
+    
     // =====================================================
     // IRQ handling stuff (internal use only)
     void handleIrq();
-
+    
     volatile uint8_t drssi; // received signal strength (see dssi table)
-
-  private:
-
+    
+private:
+    
     // =====================================================
     // Internal driver state
     enum {
-      RF_TXCRC1 = 1, // sending, first CRC byte follows => 1
-      RF_TXCRC2, // sending, second CRC byte follows    => 2
-      RF_TXTAIL1,// sending, tail follows               => 3
-      RF_TXTAIL2,// sending, switchoff follows          => 4
-      RF_TXDONE, // sending finished, have to switch off sender, falling to RF_IDLE => 5
-      RF_TXPRE,  // sending prefix tact sync            => 6
-      RF_TXSYN1, // sending, first SYNC byte            => 7
-      RF_TXSYN2, // sending, secondfirst SYNC byte, buffer follows => 8
-
-      RF_RECV,   // in receive state (may already have received some bytes)
-      RF_IDLE    // full packet received, receiving disabled, will switch to RF_RECV on next call to recvDone()
+        RF_TXCRC1 = 1, // sending, first CRC byte follows => 1
+        RF_TXCRC2, // sending, second CRC byte follows    => 2
+        RF_TXTAIL1,// sending, tail follows               => 3
+        RF_TXTAIL2,// sending, switchoff follows          => 4
+        RF_TXDONE, // sending finished, have to switch off sender, falling to RF_IDLE => 5
+        RF_TXPRE,  // sending prefix tact sync            => 6
+        RF_TXSYN1, // sending, first SYNC byte            => 7
+        RF_TXSYN2, // sending, secondfirst SYNC byte, buffer follows => 8
+        
+        RF_RECV,   // in receive state (may already have received some bytes)
+        RF_IDLE    // full packet received, receiving disabled, will switch to RF_RECV on next call to recvDone()
     };
-
+    
     volatile int8_t state;    // see enum above
     volatile uint16_t rfMode; // current mode of RFM module (0x82xx)
     uint8_t irqLine;          // where irq from RFM12b is connected to
     volatile uint8_t _toSend; // next byte to send while transmitting (RF_TX*)
-
+    
     boolean reportBroken;     // report packets with invalid checksum to Sketch?
     
-
+    
     // =====================================================
     // Network infos
     uint8_t nodeId;
     uint8_t groupId;
     uint8_t bandId;
-
-  
+    
+    
     // =====================================================
     // RFM12b infos
     boolean available;  // did module responhd to reset command
@@ -173,32 +173,32 @@ class RF12_T3 {
     uint8_t datarate;
     volatile boolean _recvDone; // a message is waiting in "buffer"
     volatile int arssi; // received signal strength (analogRead value)
-
-
+    
+    
     // =====================================================
     // Enable/Disable components / communicate to RFM12b module
     void enableReceiver();
     void disableReceiver();
     void enableTransmitter();
     void disableTransmitter();
-
+    
     inline uint16_t rf12_xfer(uint16_t data);
-
+    
     static void _handleIrq4() {
-      _instance->handleIrq();
+        _instance->handleIrq();
     }
-
-
+    
+    
     // =====================================================
     // Receive/send buffer handling
     void resetReceiveBuffer() {
-      _index = 0;
-      buffer[0] = 0; // reset header
-      buffer[1] = 0; // reset len
-      rf12_crc = 0xffff;
+        _index = 0;
+        buffer[0] = 0; // reset header
+        buffer[1] = 0; // reset len
+        rf12_crc = 0xffff;
     }
-
-
+    
+    
     // =====================================================
     // Singleton handling
     static RF12_T3* _instance;
